20e7f4e99f817b89aeb251bdef5f0596ca0b204c
jhuniorasto <jhuniorasto6@gmail.com>
2025-03-06 11:49:19 -0500

    1.1. Added CSS styles

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,30 @@
 
+
+
+
+
+var comentarios = [];
+
+function agregarComentario(){
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("correo").value;
+    let comentario = document.getElementById("comentario").value;
+
+
+    let comen = {"nombre":nombre,
+        "correo": correo,
+        "comentario":comentario
+    };
+
+    comentarios.push(comen);
+    limpiarCampos();
+
+}
+
+
+function limpiarCampos() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("correo").value = "";
+    document.getElementById("comentario").value = "";
+
+}
